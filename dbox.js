b96c16e9d5bf9b993c3662e60841c53b0da54107
Alecs <alecs.garza@akora.mx>
2016-04-02 13:53:50 -0600

    test directive angular

--- dbox.js
+++ dbox.js
@@ -8,3 +8,38 @@ window.dbox = (function () {
      
     return dbox;
 }());
+
+
+angular.module('dbox',[]); 
+
+
+	angular
+	    .module('dbox')
+	    .directive('myExample', myExample);
+
+	function myExample() {
+	    var directive = {
+	        restrict: 'E',
+	        template: '<div>Blanca por que ella manda</div>',
+	        scope: {
+	       
+	        },
+	        controller: myExampleController,
+	        controllerAs: 'vm',
+	        bindToController: true // because the scope is isolated
+	    };
+
+	    return directive;
+	}
+
+
+
+	function myExampleController() {
+	  var vm = this;
+
+	  //---------------------------------------
+	  alert('Funcionó');
+	}
+
+
+
